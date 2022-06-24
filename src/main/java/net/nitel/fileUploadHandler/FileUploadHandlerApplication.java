bae35d131e2084478e23312c5fb53c9fefc5e5a1
tumbimsiska <tmsiska@nitel.mw>
2022-06-24 11:23:57 +0200

    first com

---
+++ src/main/java/net/nitel/fileUploadHandler/FileUploadHandlerApplication.java
@@ -0,0 +1,13 @@
+package net.nitel.fileUploadHandler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileUploadHandlerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileUploadHandlerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/net/nitel/fileUploadHandler/FileUploadHandlerApplicationTests.java
@@ -0,0 +1,13 @@
+package net.nitel.fileUploadHandler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FileUploadHandlerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
